5b707dbf30a5592ea58afc1bb655c83954779124
Kate Frolova <frolkate.alex@gmail.com>
2022-08-15 16:02:10 +0300

    feat: add methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = null;
+        this.right = null;
+    }
+    
 
     setRange(min, max) {
+
+     this.left = min;
+     this.right = max;
 
     }
 
     guess() {
 
+        return Math.floor((this.right - this.left) / 2); 
+
     }
 
     lower() {
 
+        this.max = this.guess();
+        
+    
     }
 
     greater() {
+
+        this.min = this.guess();
 
     }
 }
